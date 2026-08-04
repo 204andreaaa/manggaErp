f655bb65b548f6350f255656537e2489773284ad
204andreaaa <204andreaaa@gmail.com>
2026-08-04 16:26:10 +0700

    Fix syntax error in DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,77 +2,19 @@
 
 namespace Database\Seeders;
 
-use Database\Seeders\Bom\BomItemSeeder;
-use Database\Seeders\Bom\BomSeeder;
-use Database\Seeders\Bom\BomTransactionItemSeeder;
-use Database\Seeders\Bom\BomTransactionSeeder;
-use Database\Seeders\Core\CategorySeeder;
 use Database\Seeders\Core\CompanySeeder;
-use Database\Seeders\Core\PackageSeeder;
-use Database\Seeders\Core\ProductSeeder;
 use Database\Seeders\Core\RoleSeeder;
-use Database\Seeders\Core\SupplierSeeder;
-use Database\Seeders\Core\UserSeeder;
-use Database\Seeders\Core\WarehouseSeeder;
-use Database\Seeders\Inventory\RequestRestockSeeder;
-use Database\Seeders\Inventory\StockAdjustmentSeeder;
-use Database\Seeders\Inventory\StockLevelSeeder;
-use Database\Seeders\Inventory\StockMovementSeeder;
-use Database\Seeders\Inventory\StockRequestSeeder;
-use Database\Seeders\Purchase\PurchaseOrderSeeder;
-use Database\Seeders\Purchase\RestockReceiptSeeder;
-use Database\Seeders\Sales\SalesHandoverItemSeeder;
-use Database\Seeders\Sales\SalesHandoverSeeder;
-use Database\Seeders\Sales\SalesReportSeeder;
-use Database\Seeders\Sales\SalesReturnSeeder;
-use Database\Seeders\System\ActivityLogSeeder;
-use Database\Seeders\System\OperationAdminSeeder;
-use Database\Seeders\Transfer\WarehouseTransferItemSeeder;
-use Database\Seeders\Transfer\WarehouseTransferLogSeeder;
-use Database\Seeders\Transfer\WarehouseTransferSeeder;
+use Database\Seeders\Erp\ErpSetupSeeder;
 use Illuminate\Database\Seeder;
-
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
         $this->call([
-            // CORE ERP SEEDERS
             CompanySeeder::class,
             RoleSeeder::class,
-            \Database\Seeders\Erp\ErpSetupSeeder::class,
-        ]);
-
-            /* 
-            // INVENTORY
-            StockLevelSeeder::class,
-            StockMovementSeeder::class,
-            StockRequestSeeder::class,
-            RequestRestockSeeder::class,
-            StockAdjustmentSeeder::class,
-
-            // PURCHASE
-            PurchaseOrderSeeder::class,
-            RestockReceiptSeeder::class,
-
-            // SALES
-            SalesHandoverSeeder::class,
-            SalesHandoverItemSeeder::class,
-            SalesReportSeeder::class,
-            SalesReturnSeeder::class,
-
-            // TRANSFER
-            WarehouseTransferSeeder::class,
-            WarehouseTransferItemSeeder::class,
-            WarehouseTransferLogSeeder::class,
-
-            // BOM
-            BomSeeder::class,
-            BomItemSeeder::class,
-            BomTransactionSeeder::class,
-            BomTransactionItemSeeder::class,
-            */
+            ErpSetupSeeder::class,
         ]);
     }
 }
